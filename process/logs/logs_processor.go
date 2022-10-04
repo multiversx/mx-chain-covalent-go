0201093dc1e4d0a447e8e6c6050b0801efe9e228
Elrond/ <marius.ciucam@gmail.com>
2022-10-04 13:14:00 +0300

    FEAT: First version

--- process/logs/logs_processor.go
+++ process/logs/logs_processor.go
@@ -1,57 +1,32 @@
 package logs
 
 import (
-	"github.com/ElrondNetwork/covalent-indexer-go"
-	"github.com/ElrondNetwork/covalent-indexer-go/process/utility"
-	"github.com/ElrondNetwork/covalent-indexer-go/schema"
-	"github.com/ElrondNetwork/elrond-go-core/core"
-	"github.com/ElrondNetwork/elrond-go-core/core/check"
-	"github.com/ElrondNetwork/elrond-go-core/data"
+	"github.com/ElrondNetwork/covalent-indexer-go/schemaV2"
+	"github.com/ElrondNetwork/elrond-go-core/data/transaction"
 )
 
 type logsProcessor struct {
-	pubKeyConverter core.PubkeyConverter
 }
 
 // NewLogsProcessor creates a new instance of logs processor
-func NewLogsProcessor(pubKeyConverter core.PubkeyConverter) (*logsProcessor, error) {
-	if check.IfNil(pubKeyConverter) {
-		return nil, covalent.ErrNilPubKeyConverter
-	}
-
-	return &logsProcessor{
-		pubKeyConverter: pubKeyConverter,
-	}, nil
+func NewLogsProcessor() *logsProcessor {
+	return &logsProcessor{}
 }
 
-// ProcessLogs converts logs data to a specific structure defined by avro schema
-func (lp *logsProcessor) ProcessLogs(logs []*data.LogData) []*schema.Log {
-	allLogs := make([]*schema.Log, 0, len(logs))
-
-	for _, currLog := range logs {
-		processedLog := lp.processLog(currLog)
-		if processedLog != nil {
-			allLogs = append(allLogs, processedLog)
-		}
+// ProcessLog converts logs data to a specific structure defined by avro schema
+func (lp *logsProcessor) ProcessLog(log *transaction.ApiLogs) *schemaV2.Log {
+	if log == nil {
+		return schemaV2.NewLog()
 	}
 
-	return allLogs
-}
-
-func (lp *logsProcessor) processLog(logData *data.LogData) *schema.Log {
-	if logData == nil || check.IfNil(logData.LogHandler) {
-		return nil
-	}
-
-	return &schema.Log{
-		ID:      []byte(logData.TxHash),
-		Address: utility.EncodePubKey(lp.pubKeyConverter, logData.LogHandler.GetAddress()),
-		Events:  lp.processEvents(logData.LogHandler.GetLogEvents()),
+	return &schemaV2.Log{
+		Address: []byte(log.Address),
+		Events:  lp.processEvents(log.Events),
 	}
 }
 
-func (lp *logsProcessor) processEvents(events []data.EventHandler) []*schema.Event {
-	allEvents := make([]*schema.Event, 0, len(events))
+func (lp *logsProcessor) processEvents(events []*transaction.Events) []*schemaV2.Event {
+	allEvents := make([]*schemaV2.Event, 0, len(events))
 
 	for _, currEvent := range events {
 		processedEvent := lp.processEvent(currEvent)
@@ -64,15 +39,15 @@ func (lp *logsProcessor) processEvents(events []data.EventHandler) []*schema.Eve
 	return allEvents
 }
 
-func (lp *logsProcessor) processEvent(event data.EventHandler) *schema.Event {
-	if check.IfNil(event) {
+func (lp *logsProcessor) processEvent(event *transaction.Events) *schemaV2.Event {
+	if event == nil {
 		return nil
 	}
 
-	return &schema.Event{
-		Address:    utility.EncodePubKey(lp.pubKeyConverter, event.GetAddress()),
-		Identifier: event.GetIdentifier(),
-		Topics:     event.GetTopics(),
-		Data:       event.GetData(),
+	return &schemaV2.Event{
+		Address:    []byte(event.Address),
+		Identifier: []byte(event.Identifier),
+		Topics:     event.Topics,
+		Data:       event.Data,
 	}
 }
