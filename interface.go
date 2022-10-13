725f5c3d08e47726ed060d41e5bd60d5342926de
Elrond/ <marius.ciucam@gmail.com>
2022-10-13 16:18:04 +0300

    CLN: Remove schema v1

--- interface.go
+++ interface.go
@@ -2,25 +2,13 @@ package covalent
 
 import (
 	"github.com/ElrondNetwork/covalent-indexer-go/hyperBlock"
-	"github.com/ElrondNetwork/covalent-indexer-go/schema"
 	"github.com/ElrondNetwork/covalent-indexer-go/schemaV2"
-	"github.com/ElrondNetwork/elrond-go-core/data/indexer"
-	vmcommon "github.com/ElrondNetwork/elrond-vm-common"
 	"github.com/elodina/go-avro"
 )
-
-type DataHandler interface {
-	ProcessData(args *indexer.ArgsSaveBlockData) (*schema.BlockResult, error)
-}
 
 // HyperBlockProcessor shall handle hyper block processing into avro schema blocks
 type HyperBlockProcessor interface {
 	Process(hyperBlock *hyperBlock.HyperBlock) (*schemaV2.HyperBlock, error)
-}
-
-type AccountsAdapter interface {
-	LoadAccount(address []byte) (vmcommon.AccountHandler, error)
-	IsInterfaceNil() bool
 }
 
 // AvroMarshaller defines what an avro marshaller should do
